58ed53eaa660cf00c91c0d4e56760b1e24cff542
Dibbles <daphnekodua@gmail.com>
2022-01-24 10:35:34 +0000

    Variables created

--- Madlibs.py
+++ Madlibs.py
@@ -1,1 +1,21 @@
+print("Welcome to Madlibs! ")
+food = input("Food: ")
+room_in_house = input("Room in the house: ")
+noun = input("Noun: ")
+adjective = input("Adjective: ")
+noun1 = input("Noun: ")
+noun2 = input(" Noun: ")
+adjective1 = input("Adjective: ")
+adverb = input("Adverb: ")
+different_room_in_house = input("Different room in house: ")
+
+
+
+
+print(f"Last week was my birthday. We went to my Grandma's house and had {food} for supper.\
+ After that, we all sat in the {room_in_house} to open my presents. The first gift was from my sister, and it was a\
+  {noun}. The next gift was from my mum and it was a(n) {adjective} {noun1}. The gift after that was a {noun2}.\
+ I pretended to be happy-but I already had one of those!. All of a sudden , I heard a strange noise. The noise was \
+ {adjective1},and I couldnt tell where it was coming from. I looked around the room. I stood up and walked {adverb} \
+ to the {different_room_in_house} .")
 
